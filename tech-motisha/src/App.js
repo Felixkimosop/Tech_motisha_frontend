d59e00a589ab7a499759545225aac93289bcd419
Enock-Yegon <ykibet70@gmail.com>
2023-04-18 11:54:35 +0300

    Add signup

--- tech-motisha/src/App.js
+++ tech-motisha/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
+import ContactMe from "./components/ContactMe/ContactMe";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contactme" element={<ContactMe />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
