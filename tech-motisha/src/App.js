4ef8ac92575cf692c48b0193c513d37b5cbd8ad9
Felix Kipruto <kimosopfelix@gmail.com>
2023-04-18 13:12:44 +0300

    Resolved conflict

--- tech-motisha/src/App.js
+++ tech-motisha/src/App.js
@@ -1,38 +1,35 @@
-import {Routes, Route} from 'react-router';
-import './App.css';
+import { Routes, Route } from "react-router";
+import "./App.css";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
 import ContactMe from "./components/ContactMe/ContactMe";
-import User from './Components/User/User';
-import Staff from './Components/Staff/Staff';
-import Posts from './Components/Admin/Posts';
-import Blogpage from './Components/Blogpage';
-import About from './Pages/About/About';
-import Admin from './Components/Admin/Admin';
-import ViewUsers from './Components/Admin/ViewUsers';
-import Catergory from './Components/Admin/Catergory';
-
+import User from "./Components/User/User";
+import Staff from "./Components/Staff/Staff";
+import Posts from "./Components/Admin/Posts";
+import Blogpage from "./Components/Blogpage";
+import About from "./Pages/About/About";
+import Admin from "./Components/Admin/Admin";
+import ViewUsers from "./Components/Admin/ViewUsers";
+import Catergory from "./Components/Admin/Catergory";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path = "/user" element = {<User />} />
-        <Route path = "/admin" element = {<Admin />} />
-        <Route path = "/staff" element = {<Staff />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/staff" element={<Staff />} />
 
-        <Route path = "/allusers" element = {<ViewUsers />} />
-        <Route path = "/category" element = {<Catergory />} />
-        <Route path = "/posts" element = {<Posts />} />
+        <Route path="/allusers" element={<ViewUsers />} />
+        <Route path="/category" element={<Catergory />} />
+        <Route path="/posts" element={<Posts />} />
 
-        <Route path = "/blog" element = {<Blogpage />} />
-        <Route path = "/about" element = {<About />} />
+        <Route path="/blog" element={<Blogpage />} />
+        <Route path="/about" element={<About />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/contactme" element={<ContactMe />} />
-
       </Routes>
-     
     </div>
   );
 }
