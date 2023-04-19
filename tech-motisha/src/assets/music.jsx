62f5dd28c7bfa4329b72cd19311704df44556a07
MuchinaWamburia <lucymuchina20@gmail.com>
2023-04-19 11:41:34 +0300

    assets

---
+++ tech-motisha/src/assets/music.jsx
@@ -0,0 +1,22 @@
+const musicData = [
+  {
+    id: 1,
+    title: 'The Song',
+    artist: 'Ken',
+    duration: 180,
+    src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    poster: 'https://i.scdn.co/image/ab67616d00001e02f4a2ccbe20d6d52f16816812',
+  },
+  {
+    id: 2,
+    title: 'My man',
+    artist: 'Lucy',
+    duration: 240,
+    src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+    poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
+  },
+
+];
+
+
+export default musicData;
