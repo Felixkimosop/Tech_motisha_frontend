abfa563f74d5fb9983eb05a8df155947d1f8bf25
lilly <lillyannenjoki@gmail.com>
2023-04-26 16:56:42 +0300

    blogpage

--- tech-motisha/src/Components/Blogpage.js
+++ tech-motisha/src/Components/Blogpage.js
@@ -1,69 +1,30 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from "react";
+import BlogCard from "./BlogCard";
 
+function Blogpage({ token }) {
+  const [blogs, setBlogs] = useState([]);
 
-function Blogpage() {
-  
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Introduction to React', category: 'React', content: '...' },
-    { id: 2, title: 'CSS Tricks', category: 'CSS', content: '...' },
-    { id: 3, title: 'JavaScript Best Practices', category: 'JavaScript', content: '...' },
-    
-  ]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState('');
+  useEffect(() => {
+    fetch("http://localhost:3000/pdfs", token)
+      .then((res) => res.json())
+      .then((data) => {
+        setBlogs(data);
+        console.log(data);
+      });
+  }, []);
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleCategoryChange = (event) => {
-    setSelectedCategory(event.target.value);
-  };
-
-  const handleLike = (postId) => {
-    
-  };
-
-  const filteredPosts = posts.filter((post) => {
-    if (selectedCategory && post.category !== selectedCategory) {
-      return false;
-    }
-    if (searchTerm && !post.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-      return false;
-    }
-    return true;
+  const singleBlog = blogs.map((blog, index) => {
+    return <BlogCard key={index} blog={blog} />;
   });
 
   return (
-    <div>
+    <div className="blog-page">
       <h1>Tech School Blog</h1>
 
-      <div>
-        <label htmlFor="search">Search:</label>
-        <input type="text" id="search" value={searchTerm} onChange={handleSearch} />
-
-        <label htmlFor="category">Category:</label>
-        <select id="category" value={selectedCategory} onChange={handleCategoryChange}>
-          <option value="">All</option>
-          <option value="React">React</option>
-          <option value="CSS">CSS</option>
-          <option value="JavaScript">JavaScript</option>
-         </select>
-      </div>
-        {filteredPosts.map((post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-          <button onClick={() => handleLike(post.id)}>Like</button>
-          <span>{post.likes} likes</span>
-          <button>Read more</button>
-        </div>
-      ))}
+      <div className="blogss">{singleBlog}</div>
     </div>
-  )
-  }
+  );
+}
 
 
-
-
-export default Blogpage
+export default Blogpage;
