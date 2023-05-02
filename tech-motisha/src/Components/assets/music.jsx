ddbf4ce7a99ae3d97c83ec9f5249b57fea4e2127
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-05-02 03:04:28 +0300

    updated audio page

--- tech-motisha/src/Components/assets/music.jsx
+++ tech-motisha/src/Components/assets/music.jsx
@@ -1,9 +1,17 @@
+// import React from 'react'
+// import audio1 from '../assets/audio1.png'
+// import audio2 from '../assets/audio2.jpg'
+// import audio3 from '../assets/audio3.jpg'
+// import audio4 from '../assets/audio2.png'
+// import audio5 from '../assets/AUDIO.png'
+import audio6 from '../assets/audio6.jpg'
+
 const musicData = [
   {
     id: 1,
     title: 'The Song',
     artist: 'Ken',
-    poster: 'https://i.scdn.co/image/ab67616d00001e02f4a2ccbe20d6d52f16816812',
+    poster: 'https://media.istockphoto.com/id/1128104898/photo/we-have-to-satisfy-the-audition.jpg?s=612x612&w=0&k=20&c=-WiXW12oV51Hklf3K3HFc_2ed6P6TW8uKqpuncOARJs=',
     duration: 240,
     source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
     liked: false
@@ -13,51 +21,35 @@ const musicData = [
    id: 2,
     title: 'My man',
     artist: 'Lucy',
-   duration: 240,
+    duration: 240,
     source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-    poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
+    poster: 'https://images.unsplash.com/photo-1519874179391-3ebc752241dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXVkaW98ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
     liked: false
   },
   {
-    id: 2,
+    id: 3,
      title: 'My man',
      artist: 'Lucy',
     duration: 240,
      source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-     poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
+     poster: 'https://media.istockphoto.com/id/1203056288/photo/flat-lay-studio-microphone-with-professional-headphones-on-a-pc-keyboard-black-on-a-black.jpg?s=612x612&w=0&k=20&c=qRvkNEyDggOusWPTHct_X_LPNHam2heTxqHWUK_loGQ=',
      liked: false
    },
    {
-    id: 2,
+    id: 4,
      title: 'My man',
      artist: 'Lucy',
     duration: 240,
      source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-     poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
+     poster: 'https://media.istockphoto.com/id/1321599393/photo/microphone-in-blue-room.jpg?s=612x612&w=0&k=20&c=hh1kKxqtkgkES1fuIUFOzzp0eUKUzPYiP3mWhHxZ2-4=',
    },
    {
-    id: 3,
+    id: 5,
      title: '2My man',
      artist: 'Lucy',
     duration: 240,
      source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-     poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
-   },
-   {
-    id: 4,
-     title: '3y man',
-     artist: 'Lucy',
-    duration: 240,
-     source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-     poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
-   },
-   {
-    id: 5,
-     title: '3y man',
-     artist: 'Lucy',
-    duration: 240,
-     source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-     poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
+     poster: 'https://media.istockphoto.com/id/1355368074/photo/smiling-young-woman-recording-podcast.jpg?b=1&s=170667a&w=0&k=20&c=1c6XuqFEVz-7XOqRDCAkr1xch5tS7P_dLisMuicZHNc=',
    },
    {
     id: 6,
@@ -65,33 +57,8 @@ const musicData = [
      artist: 'Lucy',
     duration: 240,
      source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-     poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
-   },
-   {
-    id: 7,
-     title: '3y man',
-     artist: 'Lucy',
-    duration: 240,
-     source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-     poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
-   },
-   {
-    id: 8,
-     title: '3y man',
-     artist: 'Lucy',
-    duration: 240,
-     source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-     poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
-   },
-   {
-    id: 9,
-     title: '3y man',
-     artist: 'Lucy',
-    duration: 240,
-     source: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-     poster: 'https://via.placeholder.com/200x200.png?text=Song+2+Poster',
-   },  
-
+     poster: 'https://images.unsplash.com/photo-1610737249253-1405407a33ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTcwfHxoZWFkcGhvbmVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+   }
 ];
 
 export default musicData;
